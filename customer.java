f5bec59c142c9056df3651cef39c85e4cd5b5d04
amrr-hamed <120127063+amrr-hamed@users.noreply.github.com>
2022-12-09 11:07:47 +0200

    Add files via upload

---
+++ customer.java
@@ -0,0 +1,176 @@
+
+import java.sql.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class customer {
+  private  String meterCode;
+   private String F_name;
+   private String L_name;
+   private int Region;
+   private String Email;
+   private String Password;
+  private  String phoneNumber;
+   private int tariff;
+    private int  consumption;
+  private  String SSN;
+      java.sql.Date  payingDate;
+
+    public customer(String meterCode, String F_name, String L_name, int Region, String Email, String Password, String phoneNumber, int tariff, int consumption, String SSN, Date payingDate) {
+        this.meterCode = meterCode;
+        this.F_name = F_name;
+        this.L_name = L_name;
+        this.Region = Region;
+        this.Email = Email;
+        this.Password = Password;
+        this.phoneNumber = phoneNumber;
+        this.tariff = tariff;
+        this.consumption = consumption;
+        this.SSN = SSN;
+        this.payingDate = payingDate;
+    }
+
+    public void setMeterCode(String meterCode) {
+        this.meterCode = meterCode;
+    }
+
+    public void setF_name(String F_name) {
+        this.F_name = F_name;
+    }
+
+    public void setL_name(String L_name) {
+        this.L_name = L_name;
+    }
+
+    public void setRegion(int Region) {
+        this.Region = Region;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setTariff(int tariff) {
+        this.tariff = tariff;
+    }
+
+    public void setConsumption(int consumption) {
+        this.consumption = consumption;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public void setPayingDate(Date payingDate) {
+        this.payingDate = payingDate;
+    }
+
+    public String getMeterCode() {
+        return meterCode;
+    }
+
+    public String getF_name() {
+        return F_name;
+    }
+
+    public String getL_name() {
+        return L_name;
+    }
+
+    public int getRegion() {
+        return Region;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getTariff() {
+        return tariff;
+    }
+
+    public int getConsumption() {
+        return consumption;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public Date getPayingDate() {
+        return payingDate;
+    }
+              
+              
+              
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
